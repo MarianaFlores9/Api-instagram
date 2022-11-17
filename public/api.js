9f16e32ec26e0c1804c6e3079f7a906b4bc26e6b
MarianaFlores9 <mfloresl2102tmp@alumnoguinda.mx>
2022-11-17 02:05:34 -0600

    api-insta

--- public/api.js
+++ public/api.js
@@ -1,15 +1,14 @@
 
-const tokes = `http:/localhost:3000/metodo`;
-const toke = 'IGQVJYUHRWV0V1amdrU0QxVUdkZAkFOSnBSRXJPOG1qUmlOZAV85Qk83NXNWZAWVWZA1dTVnU2a2x0V1VEX2NWQW9QUlRtTVdBYnNpWVJMT1EzdTk4QXF3dHp3aE0xdW96T2c1NlhyY0wwMDdZAMkVMeURwdAZDZD';
-
+function convertir(){   
+    var datos = document.getElementById('lista').value.split(',');
+    console.log(datos);
 
 const galery = document.querySelector('.galery');
 const feed = document.querySelector('.contenedor-galery');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 
-const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=15&access_token=${toke}`;
-
+const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=15&access_token=${datos}`;
 
 
 fetch(url)
@@ -42,4 +41,5 @@ function moveGalery(e){
     }else{
         feed.scrollLeft -= feed.offsetWidth;
     }
+}
 }
